a99dcb66b3398d06e358a02045778ace949a32f7
ikepon <tatsuyanoheya@gmail.com>
2017-04-30 14:49:34 +0900

    Headerを作成し画像を表示

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DangerButton from './Button/DangerButton';
-
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
